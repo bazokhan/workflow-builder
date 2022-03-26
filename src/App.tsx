3f21551cc2b7a6a28f906fa5a4b1e8b8234ddadc
Mohamed Elbaz <albaz2000eg@gmail.com>
2022-03-27 00:01:57 +0200

    add material-ui

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,20 @@ import { useCallback, useState } from "react";
 import { List, arrayMove } from "react-movable";
 import { WorkflowTemplateStep } from "./types";
 import { v4 as uuid } from "uuid";
+import {
+  ListItem,
+  List as UIList,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Checkbox,
+} from "@mui/material";
 
 const App: React.FC = () => {
   const [items, setItems] = useState<WorkflowTemplateStep[]>([]);
+  const [activeStep, setActiveStep] = useState<WorkflowTemplateStep | null>(
+    null
+  );
 
   const addItem = useCallback(() => {
     const newItem: WorkflowTemplateStep = {
@@ -20,6 +31,7 @@ const App: React.FC = () => {
       WorkflowStepUsers: [],
     };
     setItems([...items, newItem]);
+    setActiveStep(newItem);
   }, [items]);
 
   const [showDebug, setShowDebug] = useState(false);
@@ -29,34 +41,53 @@ const App: React.FC = () => {
   }, [showDebug]);
 
   return (
-    <div className="w-screen h-screen overflow-hidden border border-red-700">
-      <button className="border border-pink-700" onClick={addItem}>
-        Add item
-      </button>
-      <button className="border border-pink-700" onClick={toggleDebug}>
-        {showDebug ? "Close Debug" : "Open Debug"}
-      </button>
-      <List
-        values={items}
-        onChange={({ oldIndex, newIndex }) =>
-          setItems(arrayMove(items, oldIndex, newIndex))
-        }
-        renderList={({ children, props }) => (
-          <ul className="border border-green-600" {...props}>
-            {children}
-          </ul>
-        )}
-        renderItem={({ value, props }) => (
-          <li className="border border-blue-800" {...props}>
-            {value.Name}
-          </li>
-        )}
-      />
-      {showDebug ? (
-        <p className="border border-orange-700">
-          {JSON.stringify(items, null, 2)}
-        </p>
-      ) : null}
+    <div className="w-screen h-screen overflow-hidden border border-red-700 grid grid-cols-aside">
+      <div>
+        <button className="border border-pink-700" onClick={addItem}>
+          Add item
+        </button>
+        <button className="border border-pink-700" onClick={toggleDebug}>
+          {showDebug ? "Close Debug" : "Open Debug"}
+        </button>
+        <List
+          values={items}
+          onChange={({ oldIndex, newIndex }) =>
+            setItems(arrayMove(items, oldIndex, newIndex))
+          }
+          renderList={({ children, props }) => (
+            <UIList className="border border-green-600" {...props}>
+              {children}
+            </UIList>
+          )}
+          renderItem={({ value, props }) => (
+            <ListItem className="border border-blue-800" {...props}>
+              <ListItemButton
+                role={undefined}
+                onClick={() => setActiveStep(value)}
+                dense
+              >
+                <ListItemIcon>
+                  <Checkbox
+                    edge="start"
+                    checked={activeStep?.Id === value.Id}
+                    tabIndex={-1}
+                    disableRipple
+                  />
+                </ListItemIcon>
+                <ListItemText id={value.Id} primary={value.Name} />
+              </ListItemButton>
+            </ListItem>
+          )}
+        />
+        {showDebug ? (
+          <p className="border border-orange-700">
+            {JSON.stringify(items, null, 2)}
+          </p>
+        ) : null}
+      </div>
+      <div className="border border-orange-700">
+        {activeStep ? JSON.stringify(activeStep) : null}
+      </div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        aside: "300px 1fr",
+      },
+    },
   },
   plugins: [],
 };
